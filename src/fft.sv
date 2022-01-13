79f8800e9cc7b434900c478c40a74e98dac2cf04
Alec Vercruysse <vercruysse.alec@gmail.com>
2022-01-13 15:44:56 -0800

    initial commit

---
+++ src/fft.sv
@@ -0,0 +1,315 @@
+// the width is the bit width (e.g. if width=16, 16 real and 16 im bits).
+// the input should be width-5 to account for bit growth.
+// N_2 is log base 2 of N (points in the FFT). e.g. N_2=5 for 32-point FFT.
+module fft
+  #(parameter width=16, N_2=5)
+   (input logic                clk,
+    input logic                reset,
+    input logic                start,
+    input logic                load,
+    input logic [width-1:0]    rd, // real    read data in
+    output logic [2*width-1:0] wd, // complex write data out
+    output logic               done);
+
+   logic                       enable;  // for AGU operation
+   logic                       rdsel;   // read from RAM0 or RAM1
+   logic                       we0_agu, we0, we1; // RAMx write enable
+   logic [N_2 - 1:0]           adr0a_agu, adr0b_agu, adr0a, adr0b, adr0a_load, adr0b_load, adr0a_load_agu, adr1a, adr1b, adr1a_agu;
+   logic [N_2 - 2:0]           twiddleadr; // twiddle ROM adr
+   logic [2*width-1:0]         twiddle, a, b, writea, writeb, aout, bout, rd0a, rd0b, rd1a, rd1b, val_in;
+
+   // LOAD LOGIC
+   fft_load #(width, N_2) loader(clk, reset, load, rd, adr0a_load, adr0b_load, val_in);
+   assign adr0a_load_agu = load ? adr0a_load : adr0a_agu;
+   assign adr0b = load ? adr0b_load : adr0b_agu;
+   assign writea = load ? val_in : aout;
+   assign writeb = load ? val_in : bout;
+   assign we0    = load ?   1'b1 : we0_agu;
+
+   // AGU ENABLE LOGIC
+   always_ff @(posedge clk)
+     begin
+	if      (start) enable <= 1;
+	else if (done || reset)  enable <= 0;
+     end
+
+   // OUTPUT LOGIC
+   logic [N_2-1:0] out_idx;
+   assign wd    = N_2[0] ? rd1a : rd1b; // ram holding results depends on even-ness of log2(N-points)s?
+   assign adr0a = done ? out_idx : adr0a_load_agu;
+   assign adr1a = done ? out_idx : adr1a_agu;
+
+   always_ff @(posedge clk)
+     begin
+	if      (reset) out_idx <= 0;
+	else if (done)  out_idx <= out_idx + 1'b1;
+     end
+
+   fft_agu #(width, N_2) agu(clk, enable, reset, done, rdsel, we0_agu, adr0a_agu, adr0b_agu, we1, adr1a_agu, adr1b, twiddleadr);
+   fft_twiddleROM #(width, N_2) twiddlerom(twiddleadr, twiddle);
+
+   twoport_RAM #(width, N_2) ram0(clk, we0, adr0a, adr0b, writea, writeb, rd0a, rd0b);
+   twoport_RAM #(width, N_2) ram1(clk, we1, adr1a, adr1b,   aout,   bout, rd1a, rd1b);
+   assign a = rdsel ? rd1a : rd0a;
+   assign b = rdsel ? rd1b : rd0b;
+
+   fft_butterfly #(width) bgu(twiddle, a, b, aout, bout);
+
+endmodule // fft
+
+module fft_load
+  #(parameter width=16, N_2=5)
+   (input logic clk,
+    input logic                reset,
+    input logic                load,
+    input logic [width-1:0]    rd,
+    output logic [N_2-1:0]     adr0a_load,
+    output logic [N_2-1:0]     adr0b_load,
+    output logic [2*width-1:0] val_in);
+
+   // index of input sample
+   // note that this is assuming the address of `rd` is the same as `idx`.
+   // TODO: refactor so this assumption is not made? e.g. output `idx` to address our testbench vectors?
+   logic [N_2-1:0]             idx;
+   always_ff @(posedge clk)
+     begin
+	if (reset) begin
+	   idx <= 0;
+	end else if (load) begin
+	   idx <= idx + 1'b1;
+	end
+     end
+
+   bit_reverse #(N_2) reverseaddr(idx, adr0a_load);
+   assign adr0b_load = adr0a_load;
+
+   assign val_in = {rd, 16'b0}; // imaginary is all zeros!
+   
+endmodule // fft_load
+
+module bit_reverse
+  #(parameter N_2=5)
+   (input logic [N_2-1:0] in,
+    output logic [N_2-1:0] out);
+
+   genvar                  i;
+   generate
+      for(i=0; i<N_2; i=i+1) begin : BIT_REVERSE
+	 assign out[i] = in[N_2-i-1];
+      end
+   endgenerate
+
+endmodule // bit_reverse
+
+// 32-point FFT address generation unit
+module fft_agu
+  #(parameter width=16, N_2=5)
+   (input logic            clk,
+    input logic            enable,
+    input logic            reset,
+    output logic           done,
+    output logic           rdsel,
+    output logic           we0,
+    output logic [N_2-1:0] adr0a,
+    output logic [N_2-1:0] adr0b,
+    output logic           we1,
+    output logic [N_2-1:0] adr1a,
+    output logic [N_2-1:0] adr1b,
+    output logic [N_2-2:0] twiddleadr);
+
+   logic [N_2-1:0]         fftLevel = 0;
+   logic [N_2-1:0]         flyInd = 0;
+
+   logic [N_2-1:0]         adrA;
+   logic [N_2-1:0]         adrB;
+
+   always_ff @(posedge clk) begin
+      if (reset) begin
+         fftLevel <= 0;
+         flyInd <= 0;
+      end
+      // Increment fftLevel and flyInd
+      else if(enable === 1 & ~done) begin
+         if(flyInd < 2**(N_2 - 1) - 1) begin
+            flyInd <= flyInd + 1'd1;
+         end else begin
+            flyInd <= 0;
+            fftLevel <= fftLevel + 1'd1;
+         end
+      end
+   end
+
+   // sets done when we are finished with the fft
+   assign done = (fftLevel == (N_2));
+   calcAddr #(width, N_2) adrCalc(fftLevel, flyInd, adrA, adrB, twiddleadr);
+
+   assign adr0a = adrA;
+   assign adr1a = adrA;
+
+   assign adr0b = adrB;
+   assign adr1b = adrB;
+
+   // flips every cycle
+   assign we0 =  fftLevel[0] & enable;
+   assign we1 = ~fftLevel[0] & enable;
+
+   // flips every cycle
+   assign rdsel = fftLevel[0];
+
+endmodule // fft_agu
+
+// todo: parameterize for more than 32-point FFT.
+module calcAddr
+  #(parameter width=16, N_2=5)
+   (input logic  [N_2-1:0]    fftLevel,
+    input logic [N_2-1:0]  flyInd,
+    output logic [N_2-1:0] adrA,
+    output logic [N_2-1:0] adrB,
+    output logic [N_2-2:0] twiddleadr);
+
+   logic [N_2-1:0]         tempA;
+   logic [N_2-1:0]         tempB;
+
+   always_comb begin
+      tempA = flyInd << 1'd1;
+      tempB = tempA +  1'd1;
+      adrA = ((tempA << fftLevel) | (tempA >> (N_2 - fftLevel))) & 5'h1f;
+
+      adrB = ((tempB << fftLevel) | (tempB >> (N_2 - fftLevel))) & 5'h1f;
+      twiddleadr = ((32'hffff_fff0 >> fftLevel) & 32'hf) & flyInd;
+   end
+endmodule // calcAddr
+
+module fft_twiddleROM
+  #(parameter width=16, N_2=5)
+   (input logic  [N_2-2:0] twiddleadr, // 0 - 1023 = 10 bits
+    output logic [2*width-1:0] twiddle);
+
+   // twiddle table pseudocode: w[k] = w[k-1] * w,
+   // where w[0] = 1 and w = exp(-j 2pi/N)
+   // for k=0... N/2-1
+
+   logic [2*width-1:0]         vectors [0:2**(N_2-1)-1];
+   initial $readmemb("rom/twiddle.vectors", vectors);
+   assign twiddle = vectors[twiddleadr];
+
+endmodule // fft_twiddleROM
+
+
+// make sure the script rom/hann.py has been run with
+// the desired width! the `width` param should be equal to `q` in the script.
+// todo: test hann windowing (does the read need to be clocked)?
+module hann_lut
+  #(parameter width=16, N_2=5)
+   (input logic              clk,
+    input logic [N_2-1:0]    idx,
+    output logic [width-1:0] out);
+
+   logic [width-1:0]         vectors[2**N_2-1:0];
+   initial $readmemb("rom/hann.vectors", vectors);
+
+   always @(posedge clk)
+     out <= vectors[idx];
+
+endmodule // hann_lut
+
+
+// explicit so that it is inferred, and we control
+// the truncation of the output.
+module mult
+  #(parameter width=16)
+   (input logic signed [width-1:0]  a,
+    input logic signed [width-1:0]  b,
+    output logic signed [width-1:0] out);
+
+   logic [2*width-1:0]              untruncated_out;
+
+   assign untruncated_out = a * b;
+   assign out = untruncated_out[30:15];
+   // see slade paper. this works as long as we're not
+   // multiplying two maximum mag. negative numbers.
+
+endmodule // mult
+
+
+module complex_mult
+  #(parameter width=16)
+   (input logic [2*width-1:0]  a,
+    input logic [2*width-1:0]  b,
+    output logic [2*width-1:0] out);
+
+   logic signed [width-1:0]    a_re, a_im, b_re, b_im, out_re, out_im;
+   assign a_re = a[31:16]; assign a_im = a[15:0];
+   assign b_re = b[31:16]; assign b_im = b[15:0];
+
+   logic signed [width-1:0]    a_re_be_re, a_im_b_im, a_re_b_im, a_im_b_re;
+   mult #(width) m1 (a_re, b_re, a_re_be_re);
+   mult #(width) m2 (a_im, b_im, a_im_b_im);
+   mult #(width) m3 (a_re, b_im, a_re_b_im);
+   mult #(width) m4 (a_im, b_re, a_im_b_re);
+
+   assign out_re = (a_re_be_re) - (a_im_b_im);
+   assign out_im = (a_re_b_im) + (a_im_b_re);
+   assign out = {out_re, out_im};
+endmodule // complex_mult
+
+module fft_butterfly
+  #(parameter width=16)
+   (input logic [2*width-1:0] twiddle,
+    input logic [2*width-1:0]  a,
+    input logic [2*width-1:0]  b,
+    output logic [2*width-1:0] aout,
+    output logic [2*width-1:0] bout);
+
+   logic signed [width-1:0]    a_re, a_im, aout_re, aout_im, bout_re, bout_im;
+   logic signed [width-1:0]    b_re_mult, b_im_mult;
+   logic [2*width-1:0]         b_mult;
+
+
+   // expand to re and im components
+   assign a_re = a[2*width-1:width];
+   assign a_im = a[width-1:0];
+   
+   // perform computation
+   complex_mult #(width) twiddle_mult(b, twiddle, b_mult);
+   assign b_re_mult = b_mult[31:16];
+   assign b_im_mult = b_mult[15:0];
+
+   assign aout_re = a_re + b_re_mult;
+   assign aout_im = a_im + b_im_mult;
+
+   assign bout_re = a_re - b_re_mult;
+   assign bout_im = a_im - b_im_mult;
+
+   // pack re and im outputs
+   assign aout = {aout_re, aout_im};
+   assign bout = {bout_re, bout_im};
+   
+endmodule // fft_butterfly
+
+
+// adapted from HDL example 5.7 in Harris TB
+module twoport_RAM
+  #(parameter width=16, N_2=5)
+   (input logic                clk,
+    input logic                we,
+    input logic [N_2-1:0]      adra,
+    input logic [N_2-1:0]      adrb,
+    input logic [2*width-1:0]  wda,
+    input logic [2*width-1:0]  wdb,
+    output logic [2*width-1:0] rda,
+    output logic [2*width-1:0] rdb);
+
+   reg [2*width-1:0]           mem [2**N_2-1:0];
+
+   always @(posedge clk)
+     if (we)
+       begin
+          mem[adra] <= wda;
+          mem[adrb] <= wdb;
+       end
+
+   assign rda = mem[adra];
+   assign rdb = mem[adrb];
+
+endmodule // twoport_RAM
---
+++ src/testbenches.sv
@@ -0,0 +1,143 @@
+
+// top level 32-point FFT test.
+// loads input from          rom/test_in.memh,
+// compares output to        rom/gt_test_out.memh,
+// writes computed output to rom/test_out.memh.
+// (see "/sim/verify sim fft.ipynb" to process output)
+module fft_testbench();
+   logic clk;
+   logic start, load, done, reset;
+   logic signed [15:0] rd, expected_re, expected_im, wd_re, wd_im;
+   logic [31:0]        wd;
+   logic [31:0]        idx, out_idx, expected;
+   
+   logic [15:0]        input_data [0:31];
+   logic [31:0]        expected_out [0:31];
+
+   integer             f; // file pointer
+  
+   fft #(16, 5) dut(clk, reset, start, load, rd, wd, done);
+   
+   // clk
+   always
+     begin
+	clk = 1; #5; clk=0; #5;
+     end
+   
+   // start of test: load `input_data`, `expected_out`, open output file, reset fft module.
+   initial
+     begin
+	$readmemh("rom/test_in.memh", input_data);
+	$readmemh("rom/gt_test_out.memh", expected_out);
+        f = $fopen("rom/test_out.memh", "w"); // write computed values.
+	idx=0; reset=1; #40; reset=0;
+     end	
+
+   // increment testbench counter and derive load/start signals
+   always @(posedge clk)
+     if (~reset) idx <= idx + 1;
+     else idx <= idx;
+   assign load =  idx < 32;
+   assign start = idx === 32;
+
+   // increment output address if done, reset if restarting FFT
+   always @(posedge clk)
+     if (load) out_idx <= 0;
+     else if (done) out_idx <= out_idx + 1;
+   
+   // load/start logic
+   assign rd = load ? input_data[idx[4:0]] : 0;  // read in test data by addressing `input_data` with `idx`.
+   assign expected = expected_out[out_idx[4:0]]; // get test output by addressing `expected_out` with `idx`.
+   assign expected_re = expected[31:16];         // get real      part of `expected` (gt output)
+   assign expected_im = expected[15:0];          // get imaginary part of `expected` (gt output)
+   assign wd_re = wd[31:16];                     // get real      part of `wd` (computed output)
+   assign wd_im = wd[15:0];                      // get imaginary part of `wd` (computed output)
+
+   // if FFT is done, compare gt to computed output, and write computed output to file.
+   always @(posedge clk)
+     if (done) begin
+	if (out_idx <= 31) begin
+           $fwrite(f, "%h\n", wd);
+	   if (wd !== expected) begin
+	      $display("Error @ out_idx %d: expected %b (got %b)    expected: %d+j%d, got %d+j%d", out_idx, expected, wd, expected_re, expected_im, wd_re, wd_im);
+	   end
+	end else begin
+	   $display("Slade FFT test complete.");
+           $fclose(f);
+	end
+     end
+endmodule // fft_testbench
+
+
+module bgu_testbench #(parameter width=16)();
+   logic clk;
+   logic [2*width-1:0] twiddle;
+   logic [2*width-1:0] a;
+   logic [2*width-1:0] b;
+   logic [2*width-1:0] aout;
+   logic [2*width-1:0] bout;
+
+   initial
+     forever begin
+        clk = 1'b0; #5;
+        clk = 1'b1; #5;
+     end
+
+   initial begin
+      twiddle = 0;
+      a = 0;
+      b = 0;
+      aout = 0;
+      bout = 0; #10;
+      assert (aout===0 && bout===0) else $error("case 1 failed.");
+
+      twiddle = {16'h7FFF, 16'b0}; #10; assert(aout===0 && bout===0) else $error("case 2 failed.");
+      b = {16'h7FFF, 16'b0}; #10; assert(aout==={16'h7FFE,16'b0} && bout==={16'h8002, 16'b0}) else $error("case 3 failed.");
+
+      // real test case:
+      // real b*w out: 0xEE59 (-4520). im b*w out: 0x58C2 (22722).
+      // aout: 141 + j27382. bout:  9179 - j18062.
+      twiddle = {16'h471C, 16'h6A6C}; a={16'h1234, 16'h1234}; b={16'h3FFF, 16'h3FFF}; #10;
+      assert(aout==={16'h008C, 16'h6AF6} && bout==={16'h23DC, 16'hB972}) else $error("case 4 failed!");
+
+      $display("BGU tests complete.");
+   end
+
+   fft_butterfly dut(twiddle, a, b, aout, bout);
+
+endmodule // twiddlerom_testbench
+
+// outdated (reset signal). previously tested working.
+module agu_testbench #(parameter width=16, N_2=5)();
+
+   // inputs
+   logic clk;
+   initial
+     forever begin
+        clk = 1'b0; #5;
+        clk = 1'b1; #5;
+     end
+   logic  start;
+
+   // outputs
+   logic  done;
+   logic  rdsel;
+   logic  we0;
+   logic [N_2-1:0] adr0a;
+   logic [N_2-1:0] adr0b;
+   logic           we1;
+   logic [N_2-1:0] adr1a;
+   logic [N_2-1:0] adr1b;
+   logic [N_2-2:0] twiddleadr;
+
+   fft_agu #(width, N_2) agu(clk, start, done, rdsel, we0, adr0a, adr0b, we1, adr1a, adr1b, twiddleadr);
+
+   initial begin
+      // init inputs and outputs to zero/default
+      start = 0; done = 0; rdsel = 0; we0 = 0; adr0a = 0; adr0b = 0; we1 = 0; adr1a = 0; adr1b = 0; twiddleadr = 0; #10;
+
+      // init inputs to starting values.
+      start = 1; #10;
+   end
+
+endmodule // agu_testbench
